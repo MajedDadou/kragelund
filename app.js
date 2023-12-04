fa558bf9b89f6fe62d4e84f035a1b936061dc821
MajedDadou <91125807+MajedDadou@users.noreply.github.com>
2023-12-04 16:52:56 +0100

    upload

---
+++ app.js
@@ -0,0 +1,43 @@
+
+
+
+
+document.addEventListener('scroll', function () {
+    var nav = document.getElementById('upper-nav');
+    var visibleSection = null;
+
+    for (var i = 1; i <= 5; i++) {
+        var sectionId = 'section' + i;
+        var section = document.getElementById(sectionId);
+
+        if (section) {
+            var rect = section.getBoundingClientRect();
+
+            if (rect.top < window.innerHeight  && rect.bottom > window.innerHeight ) {
+                visibleSection = section;
+
+                // Reset the color of all watch elements
+                for (var j = 1; j <= 8; j++) {
+                    var allpicElements = document.querySelectorAll('#pic' + j);
+                    allpicElements.forEach(function (picElement) {
+                        picElement.style.color = ''; // Reset color to default
+                    });
+                }
+
+                // Set the color of current section's watch elements
+                var picElements = document.querySelectorAll('#pic' + i);
+                picElements.forEach(function (picElement) {
+                    picElement.style.color = 'red';
+                });
+
+                break; // Exit the loop if a visible section is found
+            }
+        }
+    }
+
+    if (visibleSection) {
+        nav.style.display = 'block';
+    } else {
+        nav.style.display = 'none';
+    }
+});
